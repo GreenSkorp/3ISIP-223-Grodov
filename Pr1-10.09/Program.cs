cc02d1a01acb42718a042857584a967f22d95cfd
GreenSkorp <k.e.d.2007@mail.ru>
2025-09-17 14:19:54 +0300

    Начало, принимает текст

--- Pr1-10.09/Program.cs
+++ Pr1-10.09/Program.cs
@@ -3,63 +3,88 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+namespace Rabotata {
 
-namespace Rabotata
+    /*
+     Необходимо написать программу, которая будет принимать текст от пользователя и делать над ним определённые действия.
+    Функциональные требования:
 
-/*
- Необходимо написать программу, которая будет принимать текст от пользователя и делать над ним определённые действия.
-Функциональные требования:
+        Программа принимает от пользователя минимум 100 символов
+        Подсчёт количества слов в тексте
+        Поиск самого короткого слова
+        Подсчёт количества предложений
+        Подсчёт количества гласных и согласных букв
+        Поиск самого длинного слова
+        Создание статистики по частоте встречаемости каждой буквы
+        Возможность продолжить работу с новым текстом
+        Сохранение всей статистики в список
+        Возможность вывести статистику по прошлым текстам
 
-    Программа принимает от пользователя минимум 100 символов
-    Подсчёт количества слов в тексте
-    Поиск самого короткого слова
-    Подсчёт количества предложений
-    Подсчёт количества гласных и согласных букв
-    Поиск самого длинного слова
-    Создание статистики по частоте встречаемости каждой буквы
-    Возможность продолжить работу с новым текстом
-    Сохранение всей статистики в список
-    Возможность вывести статистику по прошлым текстам
-
-    Сделать подсчет слов в тексте, не считая союзы и числа
-    Удаление букв, которые введёт пользователь. После удаления букв, необходимо сделать повторный подсчёт всех статистик по тексту
+        Сделать подсчет слов в тексте, не считая союзы и числа
+        Удаление букв, которые введёт пользователь. После удаления букв, необходимо сделать повторный подсчёт всех статистик по тексту
 
 
- */
+     */
 
 
 
     class Text
-{
-    public string OriginalText { get; set; }
-    public int TotalWords { get; set; }
-    public int WordsWithoutConjunctionsNumbers { get; set; }
-    public string ShortestWord { get; set; }
-    public string LongestWord { get; set; }
-    public int SentencesCount { get; set; }
-    public int VowelsCount { get; set; }
-    public int ConsonantsCount { get; set; }
-    public Dictionary<char, int> LetterFrequency { get; set; }
-}
+    {
+        public string OriginalText { get; set; }
+        public string[] Words { get; set; }
+        public int TotalWords { get; set; }
+        public int WordsWithoutConjunctions { get; set; }
+        public string ShortestWord { get; set; }
+        public string LongestWord { get; set; }
+        public int SentencesCount { get; set; }
+        public int VowelsCount { get; set; }
+        public int ConsonantsCount { get; set; }
+        public Dictionary<char, int> LetterFrequency { get; set; }
+
+    }
 
 
-{
     class Program
     {
     private static List<Text> history = new List<Text>();
-    private static HashSet<string> conjunctions = new HashSet<string>
+    private static string[] conjunctions = new string[]
         {
             "и", "а", "но", "да", "или", "либо", "то", "не", "ни",
             "чтобы", "как", "что", "когда", "пока", "если", "хотя",
             "потому", "так", "тоже", "также", "зато", "однако"
         };
+        private static char[] chars = new char[]
+        {
+            ',','.','!','?',':',';'
+        };
 
 
     static void Main(string[] args)
         {
-
-
-
+            NewText();
         }
-    }
+
+        static string WriteText()
+        {
+            Console.WriteLine("Введите новый текст для обработки: ");
+            string text = Console.ReadLine();
+            return text;
+        }
+
+        static void NewText()
+        {
+            Text text = new Text();
+            text.OriginalText = WriteText();
+
+
+            history.Add(text);
+            
+        }
+
+
+
 }
+
+}
+
+
