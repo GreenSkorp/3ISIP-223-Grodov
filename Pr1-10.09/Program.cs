7cf71373c77b9da954607cc9c90d55391229d103
GreenSkorp <k.e.d.2007@mail.ru>
2025-10-22 14:15:55 +0300

    123

--- Pr1-10.09/Program.cs
+++ Pr1-10.09/Program.cs
@@ -10,15 +10,15 @@ namespace Pr1_10._09
     {
         private Random random = new Random();
         private int balance = 1000; // начальный баланс
-        private Dictionary<string, int> warehouse = new Dictionary<string, int>(); // склад: деталь -> количество
+        private Dictionary<string, int> Storage = new Dictionary<string, int>(); // склад: деталь -> количество
         private Dictionary<string, int> partPrices = new Dictionary<string, int>(); // цены деталей
+        private List<Order> Orders;
         private int currentStage = 0; // текущий этап (клиент)
         private int successfulRepairs = 0; // счетчик успешных ремонтов
 
         // Штрафы и настройки
-        private const int PENALTY_REFUSAL = 50; // штраф за отказ
-        private const int PENALTY_WRONG_PART = 100; // штраф за неправильную деталь
-        private const int SERVICE_FEE = 30; // плата за работу
+        private const int Otkaz = 50; // штраф за отказ
+        private const int WrongPart = 100; // штраф за неправильную деталь
 
         private void InitializeParts()
         {
@@ -33,19 +33,21 @@ namespace Pr1_10._09
             partPrices["Стартер"] = 90;
 
             // Начальный склад
-            warehouse["Двигатель"] = 1;
-            warehouse["Тормозные колодки"] = 3;
-            warehouse["Аккумулятор"] = 2;
-            warehouse["Шины"] = 4;
-            warehouse["Фары"] = 2;
-            warehouse["Трансмиссия"] = 1;
-            warehouse["Топливный насос"] = 2;
-            warehouse["Стартер"] = 1;
+            Storage["Двигатель"] = 1;
+            Storage["Тормозные колодки"] = 3;
+            Storage["Аккумулятор"] = 2;
+            Storage["Шины"] = 4;
+            Storage["Фары"] = 2;
+            Storage["Трансмиссия"] = 1;
+            Storage["Топливный насос"] = 2;
+            Storage["Стартер"] = 1;
         }
 
 
         public void Start()
         {
+            this.InitializeParts();
+
             Console.WriteLine("=== ДОБРО ПОЖАЛОВАТЬ В АВТОСЕРВИС! ===");
             Console.WriteLine($"Начальный баланс: {balance} руб.");
             Console.WriteLine("Удачи в бизнесе!\n");
@@ -58,8 +60,9 @@ namespace Pr1_10._09
                 ShowStagePreparation();
 
                 // Генерируем клиента для этого этапа
-                Customer customer = GenerateCustomer();
-                ProcessCustomer(customer);
+
+                //Customer customer = GenerateCustomer();
+                //ProcessCustomer(customer);
 
                 // Проверяем условие окончания игры
                 if (balance < 0)
@@ -81,33 +84,47 @@ namespace Pr1_10._09
                 }
             }
         }
+        public void WriteColorLine(string txr, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(txr);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public void WriteColor(string txr, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(txr);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
         private void ShowStagePreparation()
         {
+            Console.Clear();
             Console.WriteLine($"\n=========================================");
-            Console.WriteLine($"          ЭТАП {currentStage}");
-            Console.WriteLine($"     Баланс: {balance} руб.");
-            Console.WriteLine($"=========================================");
+            WriteColorLine($"          Заказ {currentStage}", ConsoleColor.DarkYellow);
+            WriteColorLine($"     Баланс: {balance} руб.", ConsoleColor.DarkYellow);
+            Console.WriteLine($"=========================================\n");
+            OrderCheck();
 
             bool preparationComplete = false;
-
             while (!preparationComplete)
             {
-                Console.WriteLine("\nПодготовка к клиенту - выберите действие:");
+                WriteColorLine("Подготовка к клиенту - выберите действие:", ConsoleColor.DarkCyan);
                 Console.WriteLine("1 - Показать склад");
                 Console.WriteLine("2 - Закупить запчасти");
                 Console.WriteLine("3 - Перейти к клиенту");
-
+                WriteColor("Выбранный пункт: ", ConsoleColor.Yellow);
                 string choice = Console.ReadLine();
 
                 switch (choice)
                 {
                     case "1":
-                        ShowWarehouse();
+                        ShowStorage();
                         break;
                     case "2":
                         ShowPurchaseMenu();
                         break;
                     case "3":
+                        NewClient();
                         preparationComplete = true;
                         break;
                     default:
@@ -117,19 +134,125 @@ namespace Pr1_10._09
             }
         }
 
+        private void NewClient()
+        {
+            int randomIndex = random.Next(partPrices.Count);
+            string BrokenPart = partPrices.Keys.ElementAt(randomIndex);
+            int BrokenPrice = partPrices[BrokenPart];
+            Console.Clear();
+
+            Console.WriteLine($"\n=========================================");
+            WriteColorLine($"          Сломано - {BrokenPart}", ConsoleColor.DarkRed);
+            WriteColorLine($"     Выручка при выполнении: {BrokenPrice*1,4} руб.", ConsoleColor.DarkYellow);
+            Console.WriteLine($"=========================================\n");
+            bool Choise = false;
+            while (!Choise) {
+                Console.WriteLine("1. Выполнить заказ");
+                Console.WriteLine("2. Отказаться от заказа");
+
+            }
+
+            Console.ReadLine();
+
+        }
+
+        private void ShowStorage()
+        {
+            Console.WriteLine("\nСклад: Предмет -> Количество на складе");
+            foreach (KeyValuePair<string, int> pair in Storage)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+            Console.WriteLine();
+
+        }
+        private void OrderCheck()
+        {
+
+        }
+        private void ShowPurchaseMenu()
+        {
+            int i = 1;
+            Order order = new Order(currentStage + 2);
+            List<string> parts = new List<string>();
+            bool choise = false;
+            ShowStorage();
+            WriteColorLine("\nЗакупка: Предмет -> Цена", ConsoleColor.DarkYellow);
+                foreach (KeyValuePair<string, int> pair in partPrices)
+                {
+                    Console.WriteLine($"{i}. {pair.Key}: {pair.Value}");
+                    i++;
+                    parts.Add(pair.Key);
+                }
+                Console.WriteLine("0. Закончить покупку") ;
+            while (!choise)
+            {
+
+
+
+
+                WriteColor("Выбранный часть для заказа: ", ConsoleColor.Yellow);
+                int chos = Convert.ToInt32(Console.ReadLine());
+                if (chos == 0) { WriteColorLine("Покупка завершена", ConsoleColor.DarkGreen); choise = true; }
+                else
+                {
+                    Console.Write("Введите кол-во товара для покупки: "); int nums = Convert.ToInt32(Console.ReadLine()); 
+                    string part = parts[chos - 1];
+                    order.Add(part, nums);
+                    WriteColorLine($"В заказ добавлено - {part}: {nums}", ConsoleColor.DarkGreen);
+                    balance = balance - partPrices[part]*nums;
+                    Console.WriteLine($"Баланс: {balance}");
+                }
+            }
+        }
+
 
 
     }
-    class Client
+    class Order
     {
+        public int Stage;
+        public Dictionary<string, int> ordering = new Dictionary<string, int>();
+        public Order(int stage)
+        {
+            Stage = stage;
+        }
+        public void WriteColorLine(string txr, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(txr);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public void WriteColor(string txr, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(txr);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public void Add(string part, int num)
+        {
+            ordering[part] = num;
+        }
+        public void List()
+        {
+            foreach (KeyValuePair<string, int> pair in ordering)
+            {
+                WriteColorLine("Товары в заказе", ConsoleColor.Blue);
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
 
+            }
+        }
     }
+
 
 
     class Program
     {
+
         static void Main(string[] args)
         {
+            AutoService service = new AutoService();
+            service.Start();
         }
     }
 }
